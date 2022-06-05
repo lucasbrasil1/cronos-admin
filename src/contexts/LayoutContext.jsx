36237c0c25542decf33d9a31a7d589405ffdc8e9
Lucas Brasil <lucasbrasil1@hotmail.com>
2022-06-05 06:13:19 -0300

    Comportamento de item ativo no sidebar

--- src/contexts/LayoutContext.jsx
+++ src/contexts/LayoutContext.jsx
@@ -2,15 +2,25 @@ import { createContext, useState } from "react";
 
 const LayoutContext = createContext();
 
-export const LayoutProvider = ({children}) => {
+export const LayoutProvider = ({ children }) => {
     const [pageTitle, setPageTitle] = useState("Cronos");
+    const [sidebarCurrent, setSidebarCurrent] = useState();
+
+    const handleSidebarCurrent = (id) => {
+        setSidebarCurrent(id);
+    }
 
     const handlePageTitleChange = (value) => {
         setPageTitle(value);
     }
 
-    return(
-        <LayoutContext.Provider value={{pageTitle, handlePageTitleChange}}>
+    return (
+        <LayoutContext.Provider value={{
+            pageTitle,
+            handlePageTitleChange,
+            sidebarCurrent,
+            handleSidebarCurrent
+        }}>
             {children}
         </LayoutContext.Provider>
     );
